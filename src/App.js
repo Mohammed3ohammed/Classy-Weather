e11c9e60c958580ad14568f18b6cfdacffd9d949
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-04-23 16:46:05 +0200

    Create basic class component with location input and initial state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
 
-function App() {
-  return (
-    <>
-    Hallo World
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {location: "lisbon"}
+  }
+  render() {
+    return (
+      <div className="app">
+        <h1>classy Weather</h1>
+        <div>
+        <input type="text" placeholder="Search from lacation..."
+        value={this.state.location}
+        onChange={(e) => this.setState({ location: e.target.value})}
+        />
+        </div>
+        <button>Get weather</button>
+      </div>
+    )
+  }
 }
 
 export default App;
